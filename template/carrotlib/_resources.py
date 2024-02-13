cd3e084c7c036815fae195c873cfce15fa1e8e7a
blueloveTH <blueloveTH@foxmail.com>
2024-02-14 01:02:43 +0800

    Update _resources.py

--- template/carrotlib/_resources.py
+++ template/carrotlib/_resources.py
@@ -1,5 +1,6 @@
 from typing import Generic, TypeVar
 import raylib as rl
+import c
 
 from _carrotlib import _get_cjk_codepoints
 from . import g as _g
@@ -36,9 +37,22 @@ def _load_texture_scaled(path: str, scale: float):
     rl.UnloadImage(image)
     return texture
 
+def _load_square_texture(size: int):
+    raw_data = b'\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR\x00\x00\x00\x04\x00\x00\x00\x04\x08\x00\x00\x00\x00\x8c\x9a\xc1\xa2\x00\x00\x00\tpHYs\x00\x00\x0b\x13\x00\x00\x0b\x13\x01\x00\x9a\x9c\x18\x00\x00\x00\x11IDAT\x08\x99c\xfc\xcf\xc0\xc0\xc0\xc4\x80B\x00\x00\x13Q\x01\x07\x9e\xa3\x9e{\x00\x00\x00\x00IEND\xaeB`\x82'
+    data = c.struct(len(raw_data))
+    for i in range(len(raw_data)):
+        data.write_uchar(raw_data[i], offset=i)
+    image = rl.LoadImageFromMemory('.png', data.addr(), data.sizeof())
+    rl.ImageResizeNN(image.addr(), size, size)
+    texture = rl.LoadTextureFromImage(image)
+    rl.UnloadImage(image)
+    return texture
+
 # You should not modify cached resources directly!!
 load_texture = ResourceLoader[rl.Texture2D](rl.LoadTexture, rl.UnloadTexture)
 load_texture_scaled = ResourceLoader[rl.Texture2D](_load_texture_scaled, rl.UnloadTexture)
+load_square_texture = ResourceLoader[rl.Texture2D](_load_square_texture, rl.UnloadTexture)
+
 load_font_cjk = ResourceLoader[rl.Font](_load_font_cjk, rl.UnloadFont)
 load_font = ResourceLoader[rl.Font](rl.LoadFont, rl.UnloadFont)
 load_sound = ResourceLoader[rl.Sound](rl.LoadSound, rl.UnloadSound)
