06a8d0eca3a7c81bcf57858658b366d1aff46725
blueloveTH <blueloveTH@foxmail.com>
2024-03-04 21:36:08 +0800

    Update _animation.py

--- template/carrotlib/_animation.py
+++ template/carrotlib/_animation.py
@@ -1,33 +1,39 @@
 import raylib as rl
 from _carrotlib import list_assets
-from typing import Iterable
+from typing import Iterable, Literal
 
 from ._resources import load_texture
 from ._renderer import Texture2D, SubTexture2D
 
+LoopType = Literal['forward', 'ping-pong'] | None
+
 class FramedAnimation:
-    def __init__(self, frames: list[Texture2D | SubTexture2D], speed: int, loop: bool):
+    def __init__(self, frames: list[Texture2D | SubTexture2D], speed: int, loop: LoopType):
         self.frames = frames
         self.speed = speed
         self.loop = loop
 
-def load_framed_animation(path: str, speed: int, loop: bool):
+        if self.loop == 'ping-pong':
+            self.frames += self.frames[-2:0:-1]
+
+def load_framed_animation(path: str, speed: int, loop: LoopType = None):
     frames = []
     for frame in sorted(list_assets(path)):
         frames.append(load_texture(frame))
     return FramedAnimation(frames, speed, loop)
 
-def load_framed_animation_atlas(path: str, tile_size: int, tile_indices: Iterable[int], speed: int, loop: bool):
+def load_framed_animation_atlas(path: str, tile_width: int, tile_height: int, tile_indices: Iterable[int], speed: int, loop: LoopType = None):
     main_tex = load_texture(path)
-    assert main_tex.width % tile_size == 0
-    assert main_tex.height % tile_size == 0
+    assert main_tex.width % tile_width == 0
+    assert main_tex.height % tile_height == 0
     frames = []
-    tiles_per_row = main_tex.width // tile_size
+    tiles_per_row = main_tex.width // tile_width
     for i in tile_indices:
-        src_x = (i % tiles_per_row) * tile_size
-        src_y = (i // tiles_per_row) * tile_size
-        frames.append(SubTexture2D(main_tex, src_x, src_y, tile_size, tile_size))
+        src_x = (i % tiles_per_row) * tile_width
+        src_y = (i // tiles_per_row) * tile_height
+        frames.append(SubTexture2D(main_tex, src_x, src_y, tile_width, tile_height))
     return FramedAnimation(frames, speed, loop)
+
 
 class FramedAnimator:
     speed: float
@@ -62,8 +68,9 @@ class FramedAnimator:
     def update(self) -> Texture2D | SubTexture2D | None:
         if self._current_animation is None:
             return
-        dt = rl.GetFrameTime()
-        self._current_frame += self._current_animation.speed * dt * self.speed
+
+        self._current_frame += self._current_animation.speed * rl.GetFrameTime() * self.speed
+
         if self._current_frame >= len(self._current_animation.frames):
             if self._current_animation.loop:
                 self._current_frame = 0
