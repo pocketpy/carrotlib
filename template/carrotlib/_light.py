e1d466d4f61a8d799ea040f46acab65bf8071e64
blueloveTH <blueloveTH@foxmail.com>
2024-03-06 09:50:03 +0800

    Update _light.py

--- template/carrotlib/_light.py
+++ template/carrotlib/_light.py
@@ -18,7 +18,6 @@ class Lightmap:
         self.image = rl.GenImageColor(width, height, Colors.Blank)
         self.texture = rl.LoadTextureFromImage(self.image)
         # lights
-        self.global_light = GlobalLight2D(self)
         self.lights = []
     
     def new_point_light(self, x: int, y: int, radius: int) -> 'PointLight2D':
@@ -26,11 +25,10 @@ class Lightmap:
         light.x = x
         light.y = y
         light.radius = radius
-        self.lights.append(light)
         return light
 
     def update(self):
-        self.global_light.draw(self.image)
+        rl.ImageClearBackground(self.image.addr(), Colors.Black)
         for light in self.lights:
             light.draw(self.image)
 
@@ -44,6 +42,7 @@ class Light2DBase:
 
     def __init__(self, lightmap: Lightmap) -> None:
         self.lightmap = lightmap
+        lightmap.lights.append(self)
 
     def draw(self, image: rl.Image) -> None:
         raise NotImplementedError
@@ -57,6 +56,7 @@ class Light2DBase:
 
 class GlobalLight2D(Light2DBase):
     def draw(self, image: rl.Image) -> None:
+        # TODO: additive blending
         rl.ImageDrawRectangle(
             image.addr(),
             0,
@@ -75,6 +75,7 @@ class PointLight2D(Light2DBase):
     def draw(self, image: rl.Image) -> None:
         # aseprite draw circle
         # https://www.aseprite.org/docs/circle/
+        # TODO: additive blending
         rl.ImageDrawCircle(
             image.addr(),
             self.x,
