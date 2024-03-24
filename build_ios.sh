f60760a1b18bee93656e19e60051d29f5548f428
blueloveTH <blueloveth@foxmail.com>
2024-03-25 00:49:50 +0800

    some change

--- build_ios.sh
+++ build_ios.sh
@@ -1,5 +1,5 @@
 mkdir -p build/ios
 cd build/ios
 
-cmake -G Xcode -DCMAKE_TOOLCHAIN_FILE=~/ios-cmake/ios.toolchain.cmake -DPLATFORM=OS64COMBINED ../..
+cmake -G Xcode -DCMAKE_TOOLCHAIN_FILE=~/ios-cmake/ios.toolchain.cmake -DPLATFORM=OS64 ../..
 cmake --build . --config Release
