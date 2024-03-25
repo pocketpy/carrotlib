7f404971a77a4858aa2ceb18d3f20ba2402657c6
blueloveTH <blueloveTH@foxmail.com>
2024-03-25 23:14:22 +0800

    Update _material.py

--- template/carrotlib/_material.py
+++ template/carrotlib/_material.py
@@ -6,7 +6,7 @@ from . import g as _g
 
 def _compile_shader(vsCode: str = None, fsCode: str = None):
     FS_INCLUDE = """
-vec3 mix(vec3 x, vec3 y, bvec3 a)
+vec3 _mix(vec3 x, vec3 y, bvec3 a)
 {
     return vec3(a.x ? y.x : x.x, a.y ? y.y : x.y, a.z ? y.z : x.z);
 }
@@ -14,7 +14,7 @@ vec3 mix(vec3 x, vec3 y, bvec3 a)
 vec3 sRGBToLinear(vec3 rgb)
 {
 // See https://gamedev.stackexchange.com/questions/92015/optimized-linear-to-srgb-glsl
-return mix(pow((rgb + 0.055) * (1.0 / 1.055), vec3(2.4)),
+return _mix(pow((rgb + 0.055) * (1.0 / 1.055), vec3(2.4)),
             rgb * (1.0/12.92),
             lessThanEqual(rgb, vec3(0.04045)));
 }
@@ -22,7 +22,7 @@ return mix(pow((rgb + 0.055) * (1.0 / 1.055), vec3(2.4)),
 vec3 LinearToSRGB(vec3 rgb)
 {
 // See https://gamedev.stackexchange.com/questions/92015/optimized-linear-to-srgb-glsl
-return mix(1.055 * pow(rgb, vec3(1.0 / 2.4)) - 0.055,
+return _mix(1.055 * pow(rgb, vec3(1.0 / 2.4)) - 0.055,
             rgb * 12.92,
             lessThanEqual(rgb, vec3(0.0031308)));
 }
