96b1d8d6c18d69b36d1ded353f015ff7ce7f2b28
blueloveTH <blueloveth@foxmail.com>
2024-03-27 12:27:36 +0800

    Update MainActivity.kt

--- android/app/src/main/java/MainActivity.kt
+++ android/app/src/main/java/MainActivity.kt
@@ -1,51 +1,33 @@
 package game.example.android
 import android.app.NativeActivity
-import android.graphics.Rect
-import android.os.Build
 import android.os.Bundle
 import android.view.View
-import android.window.OnBackInvokedCallback
-import androidx.activity.OnBackPressedDispatcher
-import androidx.annotation.RequiresApi
-import androidx.core.view.WindowCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.WindowInsetsControllerCompat
-import kotlin.system.exitProcess
 
 class MainActivity : NativeActivity() {
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // https://developer.android.com/develop/ui/views/layout/immersive
         // https://developer.android.com/develop/ui/views/layout/edge-to-edge#system-gesture-insets
-
-        val windowInsetsController =
-            WindowCompat.getInsetsController(window, window.decorView)
-        // Configure the behavior of the hidden system bars.
-        windowInsetsController.systemBarsBehavior =
-            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-        windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
-        windowInsetsController.hide(WindowInsetsCompat.Type.statusBars())
-        windowInsetsController.hide(WindowInsetsCompat.Type.navigationBars())
     }
 
+    /**
+     * "sticky immersive" full screen mode.
+     * When user swipes from top/bottom edge, transparent status bar and navigation bar appear and slowly fade out if not used.
+     * https://developer.android.com/training/system-ui/immersive
+     */
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) hideSystemUi()
+    }
 
-
-//    override fun onWindowFocusChanged(hasFocus: Boolean) {
-//        super.onWindowFocusChanged(hasFocus)
-//
-//        if (hasFocus) {
-//            hideSystemUi()
-//        }
-//    }
-
-//    @Suppress("DEPRECATION")
-//    private fun hideSystemUi() {
-//        val decorView = window.decorView
-//        decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-//                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-//                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-//                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-//                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-//                or View.SYSTEM_UI_FLAG_FULLSCREEN)
-//    }
+    @Suppress("DEPRECATION")
+    private fun hideSystemUi() {
+        val decorView = window.decorView
+        decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_FULLSCREEN)
+    }
 }
