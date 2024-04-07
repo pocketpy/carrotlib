72122b0fd61c9a4f4a19627c62f25d3d70487347
blueloveTH <blueloveTH@foxmail.com>
2024-04-07 19:12:41 +0800

    Update build.py

--- playground/backend/build.py
+++ playground/backend/build.py
@@ -69,10 +69,9 @@ def prebuild(project: str, hardcode_assets: bool):
     # add everything in template/
     for td in os.listdir('template'):
         src = os.path.join('template', td)
-        dst = os.path.join(project, td)
+        dst = os.path.join(ANDROID_ASSETS_DIR, td)
         if os.path.exists(dst):
             shutil.rmtree(dst)
-        print(f"复制 {src} -> {dst}")
         shutil.copytree(src, dst)
     print(f"{project} 预购建成功")
 
