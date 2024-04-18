0f05829f5e57acca54e7a9365cb6ec8e17263641
blueloveTH <blueloveth@foxmail.com>
2024-04-18 19:17:04 +0800

    Update _node.py

--- template/carrotlib/_node.py
+++ template/carrotlib/_node.py
@@ -222,6 +222,16 @@ class Node:
         for i in range(len(self._coroutines)):
             self._coroutines[i] = None
 
+    def request(
+            method: Literal["GET", "POST", "PUT", "DELETE", "PATCH"],
+            url: str,
+            headers=None,
+            body: str | bytes = None,
+            timeout=None):
+        """Send an HTTP request via non-blocking I/O."""
+        import naett
+        return naett.request(method, url, headers, body, timeout)
+
 
 
 def get_node(path: str) -> Node:
