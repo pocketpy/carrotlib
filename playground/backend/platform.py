28421d9441b687f9998acb7037a5573614b4c9ff
blueloveTH <blueloveth@foxmail.com>
2024-04-22 12:24:16 +0800

    some fix

--- playground/backend/platform.py
+++ playground/backend/platform.py
@@ -1,7 +1,11 @@
 import os, sys
+from tkinter import filedialog, Tk
+
+if sys.platform == 'darwin':
+    root = Tk()
+    root.withdraw()
 
 def open_directory(title: str, initial_path: str) -> str:
-    from tkinter import filedialog
     return filedialog.askdirectory(
         initialdir=initial_path,
         title=title
